b12888073072a07ea25aa89c85d71524bb1ee173
Mitanshcodes <mitansh004@gmail.com>
2026-03-24 14:03:33 -0400

    final changes

--- HW_1/script.js
+++ HW_1/script.js
@@ -8,11 +8,12 @@ const flashcards = [
 
 // These two variables will come in handy
 let currentIndex = 0;
-let showingTerm = true;
+let showingDef = true;
+
 let flashcard_content = document.getElementById('card-content')
 let previous_button = document.getElementById('prev-btn')
 let next_button = document.getElementById('next-btn')
-let card = document.getElementById('card-content')
+let card = document.getElementById('flashcard')
 let new_term = document.getElementById('new-term')
 let new_definition = document.getElementById('new-definition')
 let add_button = document.getElementById('add-card-btn')
@@ -26,6 +27,7 @@ function displayCard() {
 // The rest of the code you will write is apart of event listeners
 
 add_button.addEventListener('click', function() {
+    
     flashcards.push({term: new_term.value, definition: new_definition.value});
 
     new_term.value = "";
@@ -34,26 +36,49 @@ add_button.addEventListener('click', function() {
 
 
 card.addEventListener('click', function() {
+    if (showingDef) {
     flashcard_content.innerHTML = flashcards[currentIndex].definition;
     console.log(flashcards[currentIndex].definition);
+    } else {
+        displayCard();
+    }
+
+    showingDef = !showingDef;
+
 })
 
 
 previous_button.addEventListener('click', function() {
-    if (currentIndex > 0) {
-    flashcard_content.innerHTML = flashcards[currentIndex - 1].term;
-    console.log(flashcards[currentIndex - 1]);
+
     currentIndex--;
+
+    flashcard_content.innerHTML = flashcards[currentIndex].term;
+    console.log(flashcards[currentIndex]);
+    
+
+
+    if (currentIndex == 0) {
+        flashcard_content.innerHTML = flashcards[0].term;
+        console.log(flashcards[0]);
+        currentIndex = flashcards.length;
     }
 }
     )
 
 next_button.addEventListener('click', function() {
-    if (currentIndex < flashcards.length - 1) {
-    flashcard_content.innerHTML = flashcards[currentIndex + 1].term;
-    console.log(flashcards[currentIndex + 1]);
+
     currentIndex++;
+
+    if (currentIndex >= flashcards.length) {
+        currentIndex = 0;
+        flashcard_content.innerHTML = flashcards[currentIndex].term;
+        console.log(flashcards[currentIndex]);
     }
+
+    flashcard_content.innerHTML = flashcards[currentIndex].term;
+    console.log(flashcards[currentIndex]);
+    
+   
 }
     )
 
